7f558d628ccda4c8b3c9ac440d9a2939b6a5d514
Vikrant Chaudhary <nasa42@gmail.com>
2025-03-01 20:42:43 +0100

    improve formatting.

--- agent/src/models/handshaker.rs
+++ agent/src/models/handshaker.rs
@@ -46,7 +46,7 @@ async fn initiate(
     subname: String,
 ) -> Result<(Arc<Relay>, String), AgentError> {
     info!(
-        "Starting handshake: {} with device name: {}, subname: {}",
+        "Starting handshake: {} with device {}/{}",
         relay.host(),
         config.device_name(),
         subname
